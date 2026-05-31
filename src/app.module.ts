395efa3381ce0f0e9daae4b83f90dd821b6ae1ef
mutimutuje483 <mutimutujehope90@gmail.com>
2026-05-31 07:45:59 +0200

    i started from scratch

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
-import { TodosModule } from './todos/todos.module';
+import { UsersModule } from './users/users.module';
 @Module({
-imports: [PrismaModule, AuthModule, TodosModule],
+imports: [AuthModule, UsersModule],
 
   controllers: [AppController],
   providers: [AppService],
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,9 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
-import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtStrategy } from './jwt.strategy';
+import { AuthService } from './auth.service';
 
 @Module({
-  imports: [
-    PassportModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '7d' },
-    }),
-  ],
-  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
+  providers: [AuthService]
 })
 export class AuthModule {}
