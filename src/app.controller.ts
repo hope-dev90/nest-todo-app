2d919ab48490a2296b4206291b0deb663b49e318
mutimutuje483 <mutimutujehope90@gmail.com>
2026-06-03 10:50:02 +0200

    locally

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,4 @@
-import { Controller, Get, Res } from '@nestjs/common';
-import { Response } from 'express';
-import { join } from 'path';
+import { Controller } from '@nestjs/common';
 
 @Controller()
-export class AppController {
-  @Get('*')
-  serveReactApp(@Res() res: Response) {
-    res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
-  }
-}
+export class AppController {}
